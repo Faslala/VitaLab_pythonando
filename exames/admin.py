cbe079061fcab1dc9ddbd4095a451f5bfc1bcecc
Faslala Assis <faslala1@gmail.com>
2024-05-15 13:19:10 -0300

    inicio da aula 3... parado para estudo mas retornarei nesse projeto.

--- exames/admin.py
+++ exames/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import TiposExames, SolicitacaoExames, PedidosExames
+from .models import TiposExames, SolicitacaoExames, PedidosExames, AcessoMedico
 
 admin.site.register(TiposExames)
 admin.site.register(SolicitacaoExames)
 admin.site.register(PedidosExames)
+admin.site.register(AcessoMedico)
 
 
